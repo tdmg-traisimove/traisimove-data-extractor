2d31536a83c88c658e8599c8a7dc3f48f0b3ad87
Io A. Daza-Dillon <io@dazadillon.io>
2024-11-23 21:56:46 -0500

    handle config

--- README.md
+++ README.md
@@ -6,3 +6,14 @@ Script to extract data into csv files
 ```shell
 pip install -r requirements.txt
 ```
+2. Create a configuration file
+```json
+{
+    "db_url": "mongodb://127.0.0.1:27017/Stage_database",
+    "studies_names": [
+        "test-study"
+    ]
+}
+```
+`db_url` is the url to the database.
+`studies_names` are the names of the studies to extract
---
+++ main.py
@@ -0,0 +1,27 @@
+import json
+import sys
+from typing import List, TypedDict
+
+from pymongo import MongoClient
+
+
+class Config(TypedDict):
+    db_url: str
+    studies_names: List[str]
+
+
+def main():
+    if len(sys.argv) > 1:
+        config_filename = sys.argv[1]
+        with open(config_filename, "r") as config_file:
+            config: Config = json.load(config_file)
+    else:
+        print("Usage: python main.py config.json")
+        exit()
+
+    client = MongoClient(config["db_url"])
+    db = client.Stage_database
+
+
+if __name__ == "__main__":
+    main()
